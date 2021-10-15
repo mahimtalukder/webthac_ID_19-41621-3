6d61fed8aeb7205e028dcd1f530598b87a12586c
mahimtalukder <78541907+mahimtalukder@users.noreply.github.com>
2021-10-15 23:18:10 +0600

    Lab task 4

---
+++ Lab task 4/Home.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <link rel="stylesheet" href="styles.css">
+    <title>
+        Registration
+    </title>
+</head>
+
+<body class="background_color">
+
+    <div class="header">
+        <p class="logo"><?php include 'header.php'; ?></p>
+        <div class="header-right">
+            <a href="login.php">Login</a>
+            <a class="active" href="#home">Home</a>
+            <a href="Registration.php">Registration</a>
+        </div>
+
+    </div>
+
+    <div class="outer_box">
+        <div class="inerBox" style="height: 500px;">
+            <h1>Welcome to MT group</h1>
+        </div>
+    </div>
+    <div class="footer">
+        <?php include 'footer.php'; ?>
+    </div>
+</body>
+
+</html>
